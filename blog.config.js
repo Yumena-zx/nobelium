08cbf129ef48f9b464c2f51c83e46036062593c4
Yumena-zx <133195667+Yumena-zx@users.noreply.github.com>
2023-06-22 15:02:48 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
+  title: 'Happy!!!',
+  author: 'Craig way',
   email: 'i@craigary.net',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
